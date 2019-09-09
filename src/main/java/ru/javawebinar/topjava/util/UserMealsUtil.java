50bde9786618fedc560686ecfde56b38bc133dea
13Black <terablack@yandex.ru>
2019-09-09 16:37:26 +0300

    HW0 reDone.

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -8,28 +8,32 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
 import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class UserMealsUtil {
     public static void main(String[] args) {
-        List<UserMeal> mealList = new ArrayList<>(Arrays.asList(
+        List<UserMeal> mealList = Arrays.asList(
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
-        ));
-        //Непонятно. Если вызвать методы в другом порядке то exceed в stream методе остается false.
-        getFilteredWithExceeded_Stream(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+        );
+
         getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+        getFilteredWithExceeded_Stream(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+
     }
 
     public static List<UserMealWithExceed> getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+        List<UserMeal> localMealList = new ArrayList<>(mealList);
         List<UserMealWithExceed> result = new ArrayList<>();
         Map<LocalDate, Integer> datesWithCalories = new HashMap<>();
 
-        ListIterator<UserMeal> iterator = mealList.listIterator();
+        ListIterator<UserMeal> iterator = localMealList.listIterator();
         while (iterator.hasNext()) {
             UserMeal userMeal = iterator.next();
             LocalDate date = userMeal.getDateTime().toLocalDate();
@@ -39,7 +43,7 @@ public class UserMealsUtil {
                 iterator.remove();
         }
 
-        for (UserMeal userMeal : mealList) {
+        for (UserMeal userMeal : localMealList) {
             boolean exceed = datesWithCalories.get(userMeal.getDateTime().toLocalDate()) < 0;
             result.add(new UserMealWithExceed(userMeal.getDateTime(), userMeal.getDescription(), userMeal.getCalories(), exceed));
         }
@@ -52,9 +56,12 @@ public class UserMealsUtil {
         Map<LocalDate, Integer> datesWithCalories = mealList.stream().
                 collect(Collectors.groupingBy(u -> u.getDateTime().toLocalDate(), Collectors.summingInt(UserMeal::getCalories)));
 
+        Predicate<UserMeal> isBetween = u -> TimeUtil.isBetween(u.getDateTime().toLocalTime(), startTime, endTime);
+        Function<UserMeal, UserMealWithExceed> morfUMWE = u -> new UserMealWithExceed(u.getDateTime(), u.getDescription(), u.getCalories(), datesWithCalories.get(u.getDateTime().toLocalDate()) > caloriesPerDay);
+
         return mealList.stream().
-                filter(u -> TimeUtil.isBetween(u.getDateTime().toLocalTime(), startTime, endTime)).
-                map(u -> new UserMealWithExceed(u.getDateTime(), u.getDescription(), u.getCalories(), datesWithCalories.get(u.getDateTime().toLocalDate()) > caloriesPerDay)).
+                filter(isBetween).
+                map(morfUMWE).
                 collect(Collectors.toList());
     }
 }
